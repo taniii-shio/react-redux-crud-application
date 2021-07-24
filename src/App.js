28e537f460d1a0cb3143b1721a80661dd968e8ef
Naoto Shioya <shioya.naoto17@gmail.com>
2021-07-24 11:17:28 +0900

    create my own boilerplate

--- src/App.js
+++ src/App.js
@@ -1,21 +1,9 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>hello,react!</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>hello,react!</p>
     </div>
   );
 }
