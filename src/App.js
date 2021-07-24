b0d9a20dc937669921eea001a0e1bbbef5af727c
Naoto Shioya <shioya.naoto17@gmail.com>
2021-07-24 14:23:35 +0900

    create class component and functional component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import React from "react";
 
-function App() {
-  return <h1>hello,react!</h1>;
-}
+// function App() {
+//   return <h1>hello,react!</h1>;
+// }
+
+const App = () => {
+  return (
+    <div>
+      <Cat></Cat>
+      <Cat></Cat>
+      <Cat></Cat>
+      <Cat></Cat>
+    </div>
+  );
+};
+
+const Cat = () => {
+  return <div>Meow!</div>;
+};
 
 export default App;
