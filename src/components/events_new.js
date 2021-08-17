db04bec72bab6be88bde2186fefcd8c758ef7254
Naoto Shioya <shioya.naoto17@gmail.com>
2021-08-17 19:38:14 +0900

    implement event new component

---
+++ src/components/events_new.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+
+//import { postEvent } from "../actions";
+
+class EventsNew extends Component {
+  render() {
+    return (
+      <>
+        <div>foo</div>
+      </>
+    );
+  }
+}
+
+//const mapDispatchToProps = { postEvent };
+
+export default connect(null, null)(EventsNew);
