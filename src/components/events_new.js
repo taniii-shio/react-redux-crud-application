4590bdd35c3b141e0e28170b1cb6c09776909d1e
Naoto Shioya <shioya.naoto17@gmail.com>
2021-08-18 10:44:40 +0900

    update

--- src/components/events_new.js
+++ src/components/events_new.js
@@ -32,7 +32,7 @@ class EventsNew extends Component {
   }
 
   render() {
-    const { handleSubmit } = this.props;
+    const { handleSubmit, pristine, submitting } = this.props;
 
     return (
       <form onSubmit={handleSubmit(this.onSubmit)}>
@@ -54,7 +54,11 @@ class EventsNew extends Component {
         </div>
 
         <div>
-          <input type="submit" value="Submit" disabled={false} />
+          <input
+            type="submit"
+            value="Submit"
+            disabled={pristine || submitting}
+          />
           <Link to="/">Cancel</Link>
         </div>
       </form>
