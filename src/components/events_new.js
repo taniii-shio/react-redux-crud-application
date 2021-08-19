fbfa9607a19cdc5cbfd4454db41cc83d74ac5d1e
Naoto Shioya <shioya.naoto17@gmail.com>
2021-08-19 13:50:08 +0900

    apply material-ui to events_new and events_show components

--- src/components/events_new.js
+++ src/components/events_new.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { Field, reduxForm } from "redux-form";
 import { Link } from "react-router-dom";
+import RaisedButton from "material-ui/RaisedButton";
+import TextField from "material-ui/TextField";
 
 import { postEvent } from "../actions";
 
@@ -19,10 +21,14 @@ class EventsNew extends Component {
     } = field;
 
     return (
-      <div>
-        <input {...input} placeholder={label} type={type} />
-        {touched && error && <span>{error}</span>}
-      </div>
+      <TextField
+        hintText={label}
+        floatingLabelText={label}
+        type={type}
+        errorText={touched && error}
+        {...input}
+        fullWidth={true}
+      />
     );
   }
 
@@ -33,6 +39,7 @@ class EventsNew extends Component {
 
   render() {
     const { handleSubmit, pristine, submitting, invalid } = this.props;
+    const style = { margin: 12 };
 
     return (
       <form onSubmit={handleSubmit(this.onSubmit)}>
@@ -53,14 +60,17 @@ class EventsNew extends Component {
           />
         </div>
 
-        <div>
-          <input
-            type="submit"
-            value="Submit"
-            disabled={pristine || submitting || invalid}
-          />
-          <Link to="/">Cancel</Link>
-        </div>
+        <RaisedButton
+          label="Submit"
+          type="submit"
+          style={style}
+          disabled={pristine || submitting || invalid}
+        />
+        <RaisedButton
+          label="Cancel"
+          style={style}
+          containerElement={<Link to="/" />}
+        />
       </form>
     );
   }
@@ -71,7 +81,6 @@ const validate = (values) => {
 
   if (!values.title) errors.title = "Enter a title, please.";
   if (!values.body) errors.body = "Enter a body, please.";
-
   return errors;
 };
 
